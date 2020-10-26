fb88bdd960a6dcfad43a8ae7ecee2c524b4b63c8
Havanmai <71115868+Havanmai@users.noreply.github.com>
2020-10-26 12:24:38 +0700

    bo sung gio hang

---
+++ Doan1/Doan1/Controllers/CartController.cs
@@ -0,0 +1,69 @@
+﻿using Doan1.Models;
+using Model.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Doan1.Controllers
+{
+    public class CartController : Controller
+    {
+        private const string CartSession = "CartSession";
+        // GET: Cart
+        public ActionResult Index()
+        {
+
+            var cart = Session[CartSession];
+            var list = new List<CartItem>();
+            if (cart != null)
+            {
+                list = (List<CartItem>)cart;
+            }
+            return View(list);
+        }
+        public ActionResult AddItem(long productId, int quantity)
+        {
+            var product = new ProductDao().ViewDetail(productId);
+            var cart = Session[CartSession];
+            if (cart != null)
+            {
+                var list = (List<CartItem>)cart;
+                if (list.Exists(x => x.Product.IdProduct== productId))
+                {
+
+                    foreach (var item in list)
+                    {
+                        if (item.Product.IdProduct == productId)
+                        {
+                            item.Quantity += quantity;
+                        }
+                    }
+                }
+                else
+                {
+                    //tạo mới đối tượng cart item
+                    var item = new CartItem();
+                    item.Product = product;
+                    item.Quantity = quantity;
+                    list.Add(item);
+                }
+                //Gán vào session
+                Session[CartSession] = list;
+            }
+            else
+            {
+                //tạo mới đối tượng cart item
+                var item = new CartItem();
+                item.Product = product;
+                item.Quantity = quantity;
+                var list = new List<CartItem>();
+                list.Add(item);
+                //Gán vào session
+                Session[CartSession] = list;
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
